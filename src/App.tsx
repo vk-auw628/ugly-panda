3a23d29343c30b3dadcd874fca0563f3cca13e41
zongweikang <zongweikang@bytedance.com>
2022-01-28 21:02:29 +0800

    chore: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UglyPanda from './imgs/uglypanda.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ugly Panda</h1>
+        <img src={UglyPanda} style={{ width: 500 }}/>
       </header>
     </div>
   );
